55b54de631411f92d7cb89c5aaf8882464c79520
makeez1337 <maksymmelnu4yk@gmail.com>
2022-01-14 15:54:41 +0200

    classwork2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Users from "./components/Users/Users";
+import React, {useEffect, useState} from "react";
+import {userServices} from "./services/user.services";
+import FilterForm from "./components/FilterForm/FilterForm";
 
 function App() {
 
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        userServices.getAll()
+            .then(value => setUsers([...value]));
+    }, [])
 
     return (
         <div className="App">
-            <Users/>
+            <FilterForm setUsers={setUsers} users={users}/>
+            <Users users={users}/>
         </div>
     );
 }
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,22 +1,9 @@
-import React, {useEffect, useState} from 'react';
-
-import {userServices} from "../../services/user.services";
 import User from "../User/User";
-import FilterForm from "../FilterForm/FilterForm";
 
-const Users = () => {
-
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        userServices.getAll()
-            .then(value => setUsers([...value]));
-    }, [])
-
+const Users = ({users}) => {
 
     return (
         <div>
-            <FilterForm setUsers={setUsers} users={users}/>
             {
                 users.map(value => {
                     return <User key={value.id} item={value}/>
