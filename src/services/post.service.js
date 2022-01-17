d958ff75f3abfac2099b6137fad4c056d9e32989
makeez1337 <maksymmelnu4yk@gmail.com>
2022-01-18 00:56:55 +0200

    homework3

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,6 +1,7 @@
 import {axiosService} from "./axios.service";
+import {urls} from "../configs/urls";
 
 export const postService = {
-    getAll: axiosService().then(res => res.data),
-    getById: axiosService(id).then(res => res.data)
+    getAll: axiosService.get(urls.posts).then(res => res.data),
+    getById: axiosService.get(urls.posts).then(res => res.data)
 }
--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,6 +1,7 @@
 import {axiosService} from "./axios.service";
+import {urls} from "../configs/urls";
 
 export const usersService = {
-    getAll: axiosService().then(res => res.data),
-    getById: axiosService(id).then(res => res.data)
+    getAll: axiosService.get(urls.users).then(res => res.data),
+    getById: axiosService.get(urls.users).then(res => res.data)
 }
