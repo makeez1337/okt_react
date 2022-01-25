626ef1d5d969988756e12046a5c5bdbbf54318e7
makeez1337 <maksymmelnu4yk@gmail.com>
2022-01-25 16:24:58 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+
+    return (
+
+        <div>
+
+        </div>
+
+    );
 }
 
 export default App;
