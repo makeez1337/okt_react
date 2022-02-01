f8e7b44d72ab62e210cd954441ca327ec1c66984
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-01 22:01:25 +0200

    changes

--- src/components/TodoTask/TodoTask.js
+++ src/components/TodoTask/TodoTask.js
@@ -7,13 +7,15 @@ const TodoTask = ({task, index}) => {
 
     const dispatch = useDispatch();
 
+
+
     const isChecked = (e) => {
         dispatch(isCheckedStatus({status: e.target.checked, id: task.id}));
     }
 
     return (
         <div>
-            <input onClick={isChecked} type="checkbox"/> {task.todo}
+            <input onClick={isChecked} type="checkbox"/> {task.status === true ? <span style={{textDecoration:'line-through'}}>{task.todo}</span> : <span>{task.todo}</span>}
             <button style={{marginLeft:'10px'}} onClick={() => dispatch(deleteTodo({index,task}))}>delete</button>
         </div>
     );
