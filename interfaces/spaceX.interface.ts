b85580a4980c6c8662e47d918af6758c227e1998
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-03 16:38:46 +0200

    typescript homework

---
+++ interfaces/spaceX.interface.ts
@@ -0,0 +1,36 @@
+export interface ISpaceX {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    },
+    links: {
+        article_link: null;
+        video_link: string;
+    },
+    rocket: IRocket
+}
+
+export interface IRocket {
+    rocket_name: string;
+    first_stage: {
+        cores: [
+            {
+                flight: number;
+                core: {
+                    reuse_count: number;
+                    status: string;
+                }
+            }
+        ]
+    },
+    second_stage: {
+        payloads: [
+            {
+                payload_type: string;
+                payload_mass_kg: number;
+                payload_mass_lbs: number;
+            }
+        ]
+    }
+}
