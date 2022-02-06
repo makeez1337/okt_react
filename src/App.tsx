d102af19a7843d6aa5bb2eeec98053224ca57ab6
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-06 16:44:33 +0200

    homework ts

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+
+import {Cars, Form} from "./components";
+
+const App: FC = () => {
+
+    return (
+
+        <div>
+            <Form/>
+            <hr/>
+            <Cars/>
+        </div>
+    );
+};
+
+export default App;
