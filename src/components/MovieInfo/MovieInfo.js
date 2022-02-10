a96dec4e4f7d92af55b2e57155e7de973abf0697
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-10 13:39:14 +0200

    new features

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useLocation} from "react-router-dom";
+import {useDispatch} from "react-redux";
 
 import {Header} from "../Header/Header";
 import css from './MovieInfo.module.css'
+import {getGenreNames} from "../../utils";
+import {getVideoThunk} from "../../store";
 
 const MovieInfo = () => {
 
     const {state} = useLocation();
+    const dispatch = useDispatch();
 
-    const {backdrop_path, genre_ids, original_language, overview, release_date, original_title, vote_average} = state
 
+    const {
+        movie: {
+            id,
+            backdrop_path,
+            genre_ids,
+            original_language,
+            overview,
+            release_date,
+            original_title
+        }
+    } = state;
+
+    const {genres} = state;
 
     const background = `https://image.tmdb.org/t/p/original${backdrop_path}`;
 
@@ -21,12 +37,26 @@ const MovieInfo = () => {
         height: '100vh',
     }
 
+    const genresNamesArr = [];
+
+    getGenreNames(genres, genre_ids, genresNamesArr);
+
+    const genreNames = genresNamesArr.toString();
+
+
+    useEffect(()=> {
+        dispatch(getVideoThunk({id}));
+    })
+
     return (
         <div>
             <Header/>
             <div style={backgroundStyle}>
                 <div className={css.movie_info}>
-                    <div className={css.header}>{original_title}</div>
+                    <div className={css.title_wrap}><span className={css.title}>{original_title}</span></div>
+                    <div className={css.release_wrap}>{release_date}, language: <span className={css.clr_orng}>{original_language}</span></div>
+                    <div className={css.genres_wrap}>Genres: <span className={css.clr_orng}>{genreNames}</span></div>
+                    <div className={css.overview_wrap}><span className={css.overview}>Overview: <span className={css.clr_orng}>{overview}</span></span></div>
                 </div>
             </div>
         </div>
