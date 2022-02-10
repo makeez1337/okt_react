0b2c938b952cde23298adf325eeef4f5c0e7e0c6
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-10 14:09:44 +0200

    added features

--- src/components/MovieInfo/MovieInfo.js
+++ src/components/MovieInfo/MovieInfo.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
-import {useLocation} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import React, {useEffect, useState} from 'react';
+import {useLocation, useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
 
 import {Header} from "../Header/Header";
 import css from './MovieInfo.module.css'
@@ -11,6 +11,10 @@ const MovieInfo = () => {
 
     const {state} = useLocation();
     const dispatch = useDispatch();
+    const {videos} = useSelector(prev => prev['movieReducer']);
+    const {movieId} = useParams();
+
+    const [videoURL, setVideoURL] = useState('');
 
 
     const {
@@ -44,9 +48,19 @@ const MovieInfo = () => {
     const genreNames = genresNamesArr.toString();
 
 
-    useEffect(()=> {
+    useEffect(() => {
         dispatch(getVideoThunk({id}));
-    })
+    }, [id]);
+
+
+    useEffect(()=>{
+        if (videos.length) {
+            const YOUTUBE_KEY = videos[0].key;
+            setVideoURL(`http://www.youtube.com/embed/${YOUTUBE_KEY}`);
+        }
+    },[movieId,id])
+
+    console.log(videoURL);
 
     return (
         <div>
@@ -54,9 +68,16 @@ const MovieInfo = () => {
             <div style={backgroundStyle}>
                 <div className={css.movie_info}>
                     <div className={css.title_wrap}><span className={css.title}>{original_title}</span></div>
-                    <div className={css.release_wrap}>{release_date}, language: <span className={css.clr_orng}>{original_language}</span></div>
+                    <div className={css.release_wrap}>{release_date}, language: <span
+                        className={css.clr_orng}>{original_language}</span></div>
                     <div className={css.genres_wrap}>Genres: <span className={css.clr_orng}>{genreNames}</span></div>
-                    <div className={css.overview_wrap}><span className={css.overview}>Overview: <span className={css.clr_orng}>{overview}</span></span></div>
+                    <div className={css.overview_wrap}><span className={css.overview}>Overview: <span
+                        className={css.clr_orng}>{overview}</span></span></div>
+                    {videoURL &&
+                        <div className={css.iframe_wrap}>
+                        <iframe width="900" height="650" src={videoURL}
+                                frameBorder="0" allowFullScreen/>
+                    </div>}
                 </div>
             </div>
         </div>
