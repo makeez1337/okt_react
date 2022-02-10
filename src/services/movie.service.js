9c93f413de28ba1bea559d29cc7eacc29179df93
makeez1337 <maksymmelnu4yk@gmail.com>
2022-02-10 13:39:00 +0200

    new service

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -4,5 +4,6 @@ import {urls} from "../constants";
 export const movieService = {
     getByPage: (page) => axiosService.get(urls.discover + urls.movie + '?page=' + page).then(res => res.data),
     getGenres: () => axiosService.get(urls.genres).then(res => res.data),
-    getByGenre: (genres,page) => axiosService.get(urls.discover+urls.movie + `?with_genres=${genres}&page=${page}`).then(res => res.data)
+    getByGenre: (genres, page) => axiosService.get(urls.discover + urls.movie + `?with_genres=${genres}&page=${page}`).then(res => res.data),
+    getVideoById: (id) => axiosService.get(urls.movie + `/${id}/videos`).then(res => res.data)
 }
